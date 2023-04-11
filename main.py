6ca5c241d77657b4b178cb808bca91692823e9f6
Patrick <64855527+Valor-boop@users.noreply.github.com>
2023-04-11 18:24:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from kmeans_cluster import kmeans_segmentation
 from pso_cluster import perform_cluster
 import numpy as np
 def main():
-    ct_image_1 = np.load('Saved_NPY_Files\R_006\R_006_94.npy')
-    ct_image_2 = np.load('Saved_NPY_Files\R_004\R_004_45.npy')
+    ct_image_1 = np.load('455_LungCT\R_006_94.npy')
+    ct_image_2 = np.load('455_LungCT\R_004_45.npy')
     kmeans_segmentation(ct_image_1)
     perform_cluster(ct_image_1)
     kmeans_segmentation(ct_image_2)
