1a87c0e57e5c0f367a3c4c3a453b6d7e7edc3cfc
Patrick <64855527+Valor-boop@users.noreply.github.com>
2023-04-10 23:44:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from KmeansTest import kmeans_segmentation
+from kmeans_cluster import kmeans_segmentation
+from pso_cluster import perform_cluster
 import numpy as np
 import matplotlib.pyplot as plt
 def main():
     ct_image = np.load('Saved_DCM_Files\R_006\R_006_94.npy')
     kmeans_segmentation(ct_image)
+    perform_cluster(ct_image)
 main()
